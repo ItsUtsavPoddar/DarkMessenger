f59ba89be2bf8aea529941187a9b6c351bdac8e3
Utsav Poddar <utsavpoddar99@gmail.com>
2023-04-04 15:00:00 +0530

    1.0.0.3 mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
-const app = express();
+const connectDB = require("./config/db");
 dotenv.config();
+connectDB();
+
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Api is running");
