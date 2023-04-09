fd723fc31e5aa87f39754eb48f01f4764ae6f77b
Utsav Poddar <utsavpoddar99@gmail.com>
2023-04-09 13:45:37 +0530

    1.0.0

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+
+app.get("/api/chat", (req, resp) => {
+  resp.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log("boooooopp Port " + PORT));
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Button } from "@chakra-ui/react";
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme="gray">Button</Button>
+    </div>
+  );
+}
+
+export default App;
